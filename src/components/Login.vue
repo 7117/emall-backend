7fecc551c458cc34ade908016daa1da30acbaaa2
7117 <qinglangsalaheiyou@163.com>
2020-06-20 18:36:55 +0800

    code

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login_container">
+        登录部分
+    </div>
+</template>>
+
+<script>
+export default {
+    
+}
+</script>
+
+// scoped 样式只在当前组件生效
+// lang支持less语法
+<style lang="less" scoped>
+.login_container{
+    background-color: #2b4b6b;
+}
+</style>
