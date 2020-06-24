63eaf6c1a9dea932804f17ef3dc46c5c2095160d
7117 <qinglangsalaheiyou@163.com>
2020-06-24 20:59:51 +0800

    code

--- src/router.js
+++ src/router.js
@@ -6,9 +6,30 @@ import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
     routes: [
+        { path: '/',component: Login },
         { path: '/login',component: Login },
         { path: '/home',component: Home }
     ]
 })
+
+// 路由导航守卫
+router.beforeEach((to,from,next) => {
+    
+    const tokenStr = window.sessionStorage.getItem("token")
+
+    if(to.path == '/login'){
+        return next();
+    }
+
+    if(!tokenStr){
+        return next('/login');
+    }
+
+    next();
+
+    
+})
+
+export default router
