9fa0f5e56c63eb92d46392c03989e8768ad30189
7117 <qinglangsalaheiyou@163.com>
2020-06-24 22:37:08 +0800

    code

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-container>
-    <el-header><el-button type="info" @click="logout">退出</el-button></el-header>
+  <el-container class="home-container">
+    <el-header>退出</el-header>
     <el-container>
       <el-aside width="200px">Aside</el-aside>
       <el-main>Main</el-main>
@@ -22,4 +22,16 @@ export default {
 
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #EAEDF1;
+}
 </style>
