3ae9eab28d9a9326b850e049985c2316cb340e64
7117 <qinglangsalaheiyou@163.com>
2020-06-24 21:52:47 +0800

    .prettierrc

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,77 +7,83 @@
       </div>
 
       <!-- 表单 -->
-      <el-form  ref="loginFormRef" :model="loginForm"  :rules="loginFormRules" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户 -->
         <el-form-item prop="username">
-          <el-input  v-model="loginForm.username" prefix-icon="iconfont icon-user">
-          </el-input>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
 
         <!-- 密码 -->
         <el-form-item prop="password">
-          <el-input  v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password">
-          </el-input>
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
 
         <el-form-item class="btns">
-          <el-button type="primary" @click="login" >登录</el-button>
-          <el-button type="info" @click="resetLoginForm" >重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
-
-
     </div>
   </div>
 </template>>
 
 <script>
 export default {
-    data(){
-        return {
-            loginForm:{
-                username:'admin',
-                password:'123456'
-            },
-            loginFormRules:{
-              username:[
-                { required: true, message: '请输入活动名称', trigger: 'blur' },
-                { min: 1, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-              ],
-              password:[
-                { required: true, message: '请输入密码', trigger: 'blur' },
-                { min: 1, max: 15, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-              ]
-            }
-        }
-    },
-    methods:{
-      resetLoginForm(){
-        this.$refs.loginFormRef.resetFields();
+  data() {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
       },
-      login(){
-        // 回调函数
-        this.$refs.loginFormRef.validate(async valid=>{
-          if(!valid){
-            return;
-          }
-          // 重新的选取变量进行赋值
-          // 问题  await与async
-          const {data:res} = await this.$http.post('login',this.loginForm);
-                    
-          if (res.meta.status !== 200 ){
-            return this.$message.error("失败");
-          }
-          
-          this.$message.success("成功");
-          // 存储token
-          window.sessionStorage.setItem("token",res.data.token);
-          // 跳转
-          this.$router.push("/home");
-        }) 
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 1, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 1, max: 15, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
       }
     }
-};
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      // 回调函数
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) {
+          return
+        }
+        // 重新的选取变量进行赋值
+        // 问题  await与async
+        const { data: res } = await this.$http.post('login', this.loginForm)
+
+        if (res.meta.status !== 200) {
+          return this.$message.error('失败')
+        }
+
+        this.$message.success('成功')
+        // 存储token
+        window.sessionStorage.setItem('token', res.data.token)
+        // 跳转
+        this.$router.push('/home')
+      })
+    }
+  }
+}
 </script>
 
 // scoped 样式只在当前组件生效
@@ -117,16 +123,15 @@ export default {
   }
 }
 
-
 .btns {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 .login_form {
-    position: absolute;
-    bottom: 0px;
-    width: 100%;
-    padding: 0px 20px;
-    box-sizing: border-box;
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  padding: 0px 20px;
+  box-sizing: border-box;
 }
 </style>
