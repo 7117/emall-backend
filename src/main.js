8c18860e4d673a846567b6e27049913a2d3731a9
7117 <qinglangsalaheiyou@163.com>
2020-06-25 17:51:57 +0800

    token

--- src/main.js
+++ src/main.js
@@ -11,6 +11,12 @@ import axios from 'axios'
 Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 挂载到vue的原型对象上面 通过this直接访问$http  从而发起ajax的请求
+axios.interceptors.request.use(config => {
+
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+
+  return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
