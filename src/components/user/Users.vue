444870f92e0300da64fee968e0efa7cad2ae728b
7117 <qinglangsalaheiyou@163.com>
2020-06-27 14:48:30 +0800

    code

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -48,6 +48,16 @@
           </template>
         </el-table-column>
       </el-table>
+
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page.sync="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total,sizes, prev, pager, next,jumper"
+        :total="total"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
@@ -80,6 +90,14 @@ export default {
       }
       this.userlist = res.data.users;
       this.total = res.data.total;
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize;
+      this.getUserList();
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage;
+      this.getUserList();
     }
   }
 };
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,9 +5,10 @@ import Vue from 'vue'
 // import { Message } from 'element-ui'
 // 简写
 import {
-    Tooltip,Switch,Table,TableColumn,Button,Form,FormItem,Input,Message,Container,Header,Aside,Main,menu,submenu,MenuItemGroup,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col
+    Pagination,Tooltip,Switch,Table,TableColumn,Button,Form,FormItem,Input,Message,Container,Header,Aside,Main,menu,submenu,MenuItemGroup,MenuItem,Breadcrumb,BreadcrumbItem,Card,Row,Col
 } from 'element-ui'
 
+Vue.use(Pagination)
 Vue.use(Tooltip)
 Vue.use(Switch)
 Vue.use(Table)
