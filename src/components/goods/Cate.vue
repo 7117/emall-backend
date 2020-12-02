c5ee0f5ff5f79d4dc8f6637bfb10ca4de412e788
7117 <qinglangsalaheiyou@163.com>
2020-12-02 16:57:17 +0800

    商品分类

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -8,18 +8,56 @@
     </el-breadcrumb>
 
     <el-card>
+      <!--      按钮-->
       <el-row>
         <el-col>
           <el-button type="primary">添加分类</el-button>
         </el-col>
       </el-row>
 
+      <!--      表格-->
+      <tree-table
+          class="treeTable"
+          :data="catelist "
+          :columns="columns"
+          show-index
+          border
+          index-text="ID"
+          :selection-type="false"
+          :expand-type="false"
+      >
+        <template slot="isok" slot-scope="scope">
+          <i class="el-icon-success" style="color: lightgreen;" v-if="scope.row.cat_deleted === false"></i>
+          <i class="el-icon-error" style="color: #2b4b6b;" v-if="scope.row.cat_deleted === true"></i>
+        </template>
+
+        <template slot="order" slot-scope="scope">
+          <el-tag v-if="scope.row.cat_level===0" size="mini" type="primary">一</el-tag>
+          <el-tag v-if="scope.row.cat_level===1" size="mini" type="warning">二</el-tag>
+          <el-tag v-if="scope.row.cat_level===2" size="mini" type="info">三</el-tag>
+        </template>
+
+        <template slot="opt" slot-scope="scope">
+          <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
+          <el-button type="warning" size="mini" icon="el-icon-edit">删除</el-button>
+        </template>
+
+      </tree-table>
+      <!--      分页-->
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-sizes="[1, 2, 5, 10]"
+          :page-size="queryInfo.pagesize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
 
     </el-card>
 
   </div>
 </template>
-
 
 <script>
 export default {
@@ -31,13 +69,42 @@ export default {
         pagesize: 5
       },
       catelist: [],
-      total:0,
+      total: 0,
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          type: 'template',
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          type: 'template',
+          template: 'order'
+        },
+        {
+          label: '操作',
+          type: 'template',
+          template: 'opt'
+        },
+      ],
     }
   },
   created() {
     this.getCateList()
   },
   methods: {
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getCateList()
+    },
     async getCateList() {
       const {data: res} = await this.$http.get('categories', {params: this.queryInfo})
 
@@ -51,3 +118,9 @@ export default {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.treeTable {
+  margin-top: 15px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
-
+import TreeTable from 'vue-table-with-tree-grid'
 // 导入axios
 import axios from 'axios'
 
@@ -13,13 +13,15 @@ axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 挂载到vue的原型对象上面 通过this直接访问$http  从而发起ajax的请求
 axios.interceptors.request.use(config => {
 
-  config.headers.Authorization = window.sessionStorage.getItem('token')
+    config.headers.Authorization = window.sessionStorage.getItem('token')
 
-  return config
+    return config
 })
 Vue.prototype.$http = axios
 
+Vue.component('tree-table', TreeTable)
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
