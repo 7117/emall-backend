22b85b37621357afca5bef837349ff3179e6c204
7117 <qinglangsalaheiyou@163.com>
2020-12-04 17:38:29 +0800

    code

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -43,15 +43,52 @@
 
       <el-tabs v-model="activeName" @tab-click="handleChangeCate">
         <el-tab-pane label="用户管理" name="many">
-          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加参数
-          </el-button>
+          <el-button @click="dialogVisible = true" type="primary" size="mini" :disabled="isBtnDisabled">添加参数</el-button>
+          <el-table :data="manyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column label="ID" type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作名称" prop="attr_name">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-share">编辑</el-button>
+                <el-button type="primary" icon="el-icon-share">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
         <el-tab-pane label="配置管理" name="only" @tab-click="handleChangeCate">
-          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加属性</el-button>
+          <el-button @click="dialogVisible = true" type="primary" size="mini" :disabled="isBtnDisabled">添加属性</el-button>
+          <el-table :data="onlyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column label="ID" type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作名称" prop="attr_name">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-share">编辑</el-button>
+                <el-button type="primary" icon="el-icon-share">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
-
     </el-card>
+
+    <el-dialog
+        @close="clearContent"
+        :title="titleText"
+        :visible.sync="dialogVisible"
+        width="50%"
+    >
+      <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="80px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="addForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -63,20 +100,47 @@ export default {
       catelist: [],
       selectedCateData: [],
       manyTableData: [],
-      onlyTableData: []
+      onlyTableData: [],
+      dialogVisible: false,
+      addForm: {
+        'attr_name': '',
+      },
+      addFormRules: {
+        attr_name: [{required: true, message: '请输入活动名称', trigger: 'blur'}]
+      },
     }
   },
   created() {
     this.getCateList()
   },
   methods: {
+    addInfo() {
+      this.$refs.addFormRef.validate(async valid => {
+        if (!valid) return;
+
+        const {data: res} = await this.$http.post('categories/' + this.cateId + '/attributes', {
+          attr_name: this.addForm.attr_name,
+          attr_sel: this.activeName
+        })
+
+        if (res.meta.status !== 201) {
+          return this.$message.error("失败")
+        }
+
+        this.$message.success("成功")
+        this.dialogVisible = false
+        this.getCateList()
+
+      })
+    },
+    clearContent() {
+      this.$refs.addFormRef.resetFields()
+    },
     async handleChangeCate() {
 
       const {data: res} = await this.$http.get('categories/' + this.cateId + '/attributes', {
         params: {sel: this.activeName}
       })
-      console.log(this.activeName)
-      console.log(res.data)
 
       if (res.meta.status !== 200) {
         return this.$message.error("失败")
@@ -114,6 +178,13 @@ export default {
         return this.selectedCateData[this.selectedCateData.length - 1]
       } else {
         return null
+      }
+    },
+    titleText() {
+      if (this.activeName === 'many') {
+        return '动态参数'
+      } else {
+        return '静态参数'
       }
     },
   }
