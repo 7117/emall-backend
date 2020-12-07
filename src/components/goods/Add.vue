0ac46adc58ec3a5d475629b81e43f9a8ffde5a8b
7117 <qinglangsalaheiyou@163.com>
2020-12-07 23:51:52 +0800

    1

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -6,7 +6,7 @@
       <el-breadcrumb-item>商品列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card>
-      <el-alert title="成功提示的文案" type="success" center show-icon :closable="false"></el-alert>
+      <el-alert title="成功提示的文案" type="success" center show-icon :closable="true"></el-alert>
 
       <el-steps :active="activeIndex - 0" finish-status="success" align-center>
         <el-step title="步骤 1"></el-step>
@@ -18,13 +18,18 @@
       </el-steps>
 
 
-      <el-tabs v-model="activeIndex" :tab-position="'left'" style="height: 200px;">
-        <el-tab-pane label="用户管理" name="0">用户管理</el-tab-pane>
-        <el-tab-pane label="配置管理" name="1">配置管理</el-tab-pane>
-        <el-tab-pane label="角色管理" name="2">角色管理</el-tab-pane>
-        <el-tab-pane label="定时任务" name="3">定时任务</el-tab-pane>
-        <el-tab-pane label="定时任务" name="4">定时任务</el-tab-pane>
-      </el-tabs>
+      <el-form label-position="top" :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px">
+        <el-form-item label="活动名称" prop="name">
+          <el-input v-model="addForm.name"></el-input>
+        </el-form-item>
+        <el-tabs v-model="activeIndex" :tab-position="'left'" style="height: 200px;">
+          <el-tab-pane label="用户管理" name="0">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理" name="1">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理" name="2">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务" name="3">定时任务</el-tab-pane>
+          <el-tab-pane label="定时任务" name="4">定时任务</el-tab-pane>
+        </el-tabs>
+      </el-form>
 
     </el-card>
   </div>
@@ -34,6 +39,8 @@
 export default {
   data() {
     return {
+      addFormRules: {},
+      addForm: {},
       activeIndex: '0'
     }
   },
