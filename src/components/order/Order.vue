bd7aae801c5da61a262dca5801dc66476d58a66d
7117 <qinglangsalaheiyou@163.com>
2020-12-16 00:14:51 +0800

    1

---
+++ src/components/order/Order.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input clearable placeholder="请输入内容" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
