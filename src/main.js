5ac3477e8acb0236fd2595f222e0dc841d9255e2
7117 <qinglangsalaheiyou@163.com>
2020-12-17 10:57:12 +0800

    1

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ import 'quill/dist/quill.bubble.css' // for bubble theme
 Vue.use(VueQuillEditor, /* { default global options } */)
 // 导入axios
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 Vue.prototype.$http = axios
 
@@ -22,14 +24,17 @@ Vue.config.productionTip = false
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 挂载到vue的原型对象上面 通过this直接访问$http  从而发起ajax的请求
 axios.interceptors.request.use(config => {
-
+    //展示进度条
+    NProgress.start()
     config.headers.Authorization = window.sessionStorage.getItem('token')
-
     return config
 })
 
-
-
+axios.interceptors.response.use(config => {
+    //隐藏进度条
+    NProgress.done()
+    return config
+})
 
 Vue.component('tree-table', TreeTable)
 
