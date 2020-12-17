f8b921c233f6e6b3d1370c714f92c349f6225e39
7117 <qinglangsalaheiyou@163.com>
2020-12-17 11:36:52 +0800

    1

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,21 @@
+const prodPlugins = []
+
+if(process.env.MODE_ENV === 'production'){
+    prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
-  "presets": [
-    "@vue/cli-plugin-babel/preset"
-  ],
-  "plugins": [
-    [
-      "component",
-      {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
+    "presets": [
+        "@vue/cli-plugin-babel/preset"
+    ],
+    "plugins": [
+        [
+            "component",
+            {
+                "libraryName": "element-ui",
+                "styleLibraryName": "theme-chalk"
+            }
+        ],
+        ...prodPlugins
     ]
-  ]
 }
