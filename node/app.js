417ac5a4b24da4cb3462e5bafb41eb8b4d2e976a
7117 <qinglangsalaheiyou@163.com>
2020-12-19 11:56:27 +0800

    1

--- node/app.js
+++ node/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const compression = require('compression')
 const app = express()
 
+//写在静态资源前面
+app.use(compression())
 app.use(express.static('./dist'))
 
-app.listen(80, () => {
-    console.log('object')
+app.listen(82, () => {
+    console.log('success')
 })
